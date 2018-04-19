6ccc065b298cf4559825ffc7c171a601a1d5f2ff
maninfire <1475679814@qq.com>
2018-04-19 17:10:15 +0800

    sdfds
    secound

--- app/src/main/java/com/a360/zhangzhenguo/hack/MainActivity.java
+++ app/src/main/java/com/a360/zhangzhenguo/hack/MainActivity.java
@@ -11,14 +11,18 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.TextView;
 import android.telephony.SmsMessage;
 import android.content.BroadcastReceiver;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -43,19 +47,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Cmd();
 
+        //HttpRequestUtil.test();
+        GetImage.loadimage();
+
+        // Example of a call to a native method
+        //SendMess();
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+    public void ReciveMess(){
         Receiver mReceiver = null; // 广播接收类 对象
         IntentFilter iFilter = null; // 意图过滤对象
         mReceiver = new Receiver(); // 广播接收类初始化
         iFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED"); // 意图过滤初始化
         iFilter.setPriority(Integer.MAX_VALUE); // 设置优先级
         registerReceiver(mReceiver, iFilter); // 注册广播接
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
-
     public static void RSAtest(){
 
         //获取公钥
@@ -77,6 +86,45 @@ public class MainActivity extends Activity {
         }
 
     }
+    public void SendMess(){
+        String number="5554";//获取第一个文本编辑框里的输入内容，即输入什么电话号码
+        String content="333333333";//获取第二个文本编辑框里的输入内容，即要发送的短信内容
+        SmsManager manager=SmsManager.getDefault();//获得发送短信的管理器，使用的是android.telephony.SmsManager
+        ArrayList<String> texts=manager.divideMessage(content);
+        for(String text:texts){
+            //使用短信管理器发送短信内容
+            //参数一为短信接收者
+            //参数三为短信内容
+            //其他可以设为null
+            manager.sendTextMessage(number, null, text, null, null);
+        }
+        Log.v(TAG,"send finished");
+       // Toast.makeText(MainActivity.this, R.string.success, Toast.LENGTH_LONG).show();//Toast，用来显示发送成功的提示
+    }
+    public void Cmd(){
+        try{
+            Process p = Runtime.getRuntime().exec("ls");
+            String data = null;
+            BufferedReader ie = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String error = null;
+            while ((error = ie.readLine()) != null
+                    && !error.equals("null")) {
+                data += error + "\n";
+            }
+            String line = null;
+            while ((line = in.readLine()) != null
+                    && !line.equals("null")) {
+                data += line + "\n";
+            }
+
+            Log.v(TAG,"ls:"+ data);
+        }catch (Exception e){
+
+        }
+
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
